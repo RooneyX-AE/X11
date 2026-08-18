cb233367782a4829d980f4fd093343215a4ebd68
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-18 17:00:59 +0700

    Add boot diagnostics and BootInfo verification

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,12 +1,23 @@
 #![no_std]
 #![no_main]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+mod serial;
 
 use bootloader_api::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    serial::init();
+    serial::write_str("X11-OS: kernel entry reached\r\n");
+    serial::write_str("X11-OS: BootInfo received\r\n");
+    serial::write_str("X11-OS: memory regions = ");
+    serial::write_usize(boot_info.memory_regions.len());
+    serial::write_str("\r\n");
+    serial::write_str("X11-OS: entering idle state\r\n");
+
     loop {
         core::hint::spin_loop();
     }
@@ -14,6 +25,8 @@ fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    serial::write_str("X11-OS: KERNEL PANIC\r\n");
+
     loop {
         core::hint::spin_loop();
     }
