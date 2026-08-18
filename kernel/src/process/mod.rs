3f52ebf2d1cd34c28b32497ee232ee979555119e
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-19 05:48:47 +0700

    Export transactional userspace loader

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -7,7 +7,9 @@
 mod address_space;
 mod image;
 mod load_plan;
+mod loader;
 
 pub use address_space::{AddressSpaceError, AddressSpaceId, AddressSpaceSpec};
 pub use image::{ElfError, ElfImage, LoadSegment};
 pub use load_plan::{LoadPlan, LoadPlanError, SegmentMapping};
+pub use loader::{map_load_plan, LoadError, LoadResult};
