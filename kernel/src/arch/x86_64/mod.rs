7c0b7a627a162971213ca73d11fe85b7ca3ae2b0
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-18 18:02:53 +0700

    kernel: expose interrupt hardware boundaries

--- kernel/src/arch/x86_64/mod.rs
+++ kernel/src/arch/x86_64/mod.rs
@@ -2,11 +2,14 @@
 
 pub mod acpi;
 pub mod apic;
+pub mod ioapic;
+pub mod irq_routing;
+pub mod local_apic;
 mod gdt;
 mod idt;
-pub mod local_apic;
 pub mod page_table;
 pub mod paging;
+pub mod pic;
 
 pub fn init() {
     gdt::init();
