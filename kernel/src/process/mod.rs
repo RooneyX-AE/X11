f117b5954d214a9e81f500984aa5ea5d5d509b99
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-19 05:58:59 +0700

    Export initial userspace context contract

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -6,6 +6,7 @@
 
 mod address_space;
 mod image;
+mod initial_context;
 mod load_plan;
 mod loader;
 mod populate;
@@ -13,6 +14,7 @@ mod stack;
 
 pub use address_space::{AddressSpaceError, AddressSpaceId, AddressSpaceSpec};
 pub use image::{ElfError, ElfImage, LoadSegment};
+pub use initial_context::{InitialContext, InitialContextError};
 pub use load_plan::{LoadPlan, LoadPlanError, SegmentMapping};
 pub use loader::{map_load_plan, LoadError, LoadResult, MappedPage, MAX_MAPPED_PAGES};
 pub use populate::{populate_image, ImagePageWriter, PopulateError};
