451e9ac64c027c478fbf60378c1a05fc6622150b
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-18 16:58:17 +0700

    Adopt stable bootloader kernel entry contract

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+use bootloader_api::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
-pub extern "C" fn kernel_main() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
     loop {
         core::hint::spin_loop();
     }
