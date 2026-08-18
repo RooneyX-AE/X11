fb248bb96a97c330bbbf87594296db763ac3559b
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-18 18:24:07 +0700

    kernel: expose x86 execution state adapter

---
+++ kernel/src/arch/x86_64/mod.rs
@@ -0,0 +1,25 @@
+//! x86_64-specific CPU and platform initialization.
+
+pub mod acpi;
+pub mod apic;
+pub mod context_switch;
+pub mod ioapic;
+pub mod irq_routing;
+pub mod lapic_timer;
+pub mod local_apic;
+pub mod scheduler;
+pub mod tsc;
+mod gdt;
+mod idt;
+pub mod page_table;
+pub mod paging;
+pub mod pic;
+
+pub fn init() {
+    gdt::init();
+    idt::init();
+}
+
+pub fn timer_ticks() -> u64 {
+    idt::timer_ticks()
+}
