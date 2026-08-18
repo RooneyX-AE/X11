e13570ad03d2a6a9a22ed61b8cf309d862626827
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-19 05:18:13 +0700

    memory: export user page access capability contract

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -18,7 +18,7 @@ pub use address_space::{VirtRange, KERNEL_SPACE_START, USER_SPACE_START};
 pub use boot::MemorySummary;
 pub use frame::{EarlyFrameAllocator, FrameAllocator};
 pub use page::{Page4K, PAGE_SIZE_4K};
-pub use page_table::{MappingError, MappingFlush, PageTableMapper};
+pub use page_table::{MappingError, MappingFlush, PageAccess, PageTableMapper};
 pub use physical::PhysicalMemoryMapping;
 pub use user::{validate_slice, UserRangeError};
 
