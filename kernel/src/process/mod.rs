b47a77a126f75eb8a53f84e17cd5aaf8cb77dd1e
Axion_0207 <hoangdang02082011@gmail.com>
2026-08-19 23:31:58 +0700

    Preserve process API re-exports without warning blockers

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -23,19 +23,28 @@ mod state;
 mod user_launch;
 
 pub use address_space::{AddressSpaceError, AddressSpaceId, AddressSpaceSpec};
+#[allow(unused_imports)]
 pub use address_space_builder::{build_address_space, AddressSpaceBuildError, BuiltAddressSpace, MappedStackPage};
 pub use identity::{ProcessExecutionBinding, ProcessId};
 pub use image::{ElfError, ElfImage, LoadSegment};
 pub use image_state::{ProcessImage, ProcessImageError};
+#[allow(unused_imports)]
 pub use initial_context::{InitialContext, InitialContextError};
+#[allow(unused_imports)]
 pub use load_plan::{LoadPlan, LoadPlanError, SegmentMapping};
+#[allow(unused_imports)]
 pub use loaded_address_space::{LoadedAddressSpace, LoadedAddressSpaceError};
 pub use loader::{map_load_plan, LoadError, LoadResult, MappedPage, MAX_MAPPED_PAGES};
+#[allow(unused_imports)]
 pub use manager::{ProcessManager, ProcessManagerError, SpawnedProcess, MAX_PROCESSES};
 pub use populated_address_space::{PopulatedAddressSpace, PopulationError};
 pub use populate::{populate_image, ImagePageWriter, PopulateError};
+#[allow(unused_imports)]
 pub use ramdisk_image::{build_process_image, RamdiskImageError};
+#[allow(unused_imports)]
 pub use scheduler_binding::{bind_and_ready, ProcessSchedulerBindError};
 pub use stack::{StackPlanError, UserStackPlan};
+#[allow(unused_imports)]
 pub use state::{ExitedProcess, ProcessState, ProcessTransitionError, ReadyProcess, RunningProcess};
+#[allow(unused_imports)]
 pub use user_launch::{UserLaunchError, UserLaunchPlan};
